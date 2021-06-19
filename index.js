0213c8729b348f29d3e5b2b2302038b8f0872833
headflake <68473680+headflake@users.noreply.github.com>
2021-06-20 01:46:41 +0530

    firdt
    df

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const socket = require('socket.io')
+
+// app setup
+
+const app = express();
+const server = app.listen(2001 ,  ()=>{console.log("listening on port : 2001")})
+
+
+app.get('/crad', (req , res)=> {
+    res.send('crad.mp3')
+})
+
+app.use(express.static('public'));
+
+
+
+    let usercount = 0
+
+
+//socket setup
+
+const io = socket(server);
+
+io.on('connection' , (socket)=>{
+    console.log('handshake done '  ,socket.id,)
+    usercount ++
+    io.sockets.emit('newconnection' , (usercount + " active users"))
+    // console.log('i sent it bruh')
+    console.log(usercount)
+     
+  
+  socket.on('chat' ,(data)=>{
+      console.log(data)
+      io.sockets.emit('chat', data)
+     
+  })
+
+  socket.on('typing', (data)=>{
+
+    socket.broadcast.emit('typing' , data)
+
+});
+
+socket.on('realtime', (data)=>{
+
+    socket.broadcast.emit('realtime' , data)
+
+});
+
+socket.on('disconnect', ()=>{
+    usercount--
+    io.sockets.emit('newconnection' , (usercount + " active users"))
+    console.log(usercount)
+})
+
+
+})
+// listen for vents
+
---
+++ public/chat.js
@@ -0,0 +1,121 @@
+// make Connnection
+
+import {io} from 'socket.io-client'
+
+const socket = io.connect('http://192.168.0.131:2001/')
+
+
+//Query dom
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      feedback = document.getElementById('feedback')
+      realtime = document.getElementById('realtime')
+      usercount = document.getElementById('usercount')
+
+
+//emitter events
+
+var send = new Audio();
+send.src="send.mp3"
+
+var receive = new Audio();
+receive.src="receive.mp3"
+
+
+var type = new Audio();
+type.src="type.mp3"
+    
+
+
+
+btn.addEventListener('click', ()=>{
+   
+
+    socket.emit('chat' , {
+
+        message: message.value,
+        handle:  handle.value
+
+    })
+    message.value=""
+    send.play()
+})
+
+message.addEventListener('keypress' , ()=>{
+    socket.emit('typing' , handle.value);
+    console.log('im emiting ')
+    
+    
+    
+})
+
+
+message.addEventListener('input', ()=>{
+    console.log('from ere ' + message.value)
+    socket.emit('realtime' , ( message.value));
+})
+message.addEventListener('keypress', (e)=>{
+
+    if(e.key == "Enter"){
+        send.play()
+        socket.emit('chat' , {
+
+            message: message.value,
+            handle:  handle.value
+            
+    
+        })
+        message.value=""
+
+    }
+    
+})
+
+
+
+
+
+// receivers
+
+socket.on('chat' ,(data)=>{
+    // console.log(data)
+    feedback.innerText= ""
+    output.innerHTML+= "<p><strong>" + data.handle +  ": </strong>" + data.message+  " </p>"
+    realtime.innerHTML= ""
+    receive.play()
+    
+
+   
+    
+    
+    
+    if(handle.value){
+        localStorage.setItem("username" , handle.value)
+        handle.style.display ='none'
+    
+    }
+    else if(localStorage.getItem("username")){
+        handle.value = localStorage.getItem("username")
+    }
+})
+
+socket.on('typing', (data)=>{
+    feedback.innerHTML= "<p><em>"+data+ "  is typing...." +"</em> </p>"
+    console.log('im getting ')
+    type.play()
+})
+
+
+
+
+socket.on('realtime' ,(data)=>{
+    console.log(data)
+    realtime.innerHTML= "<p>"+data+ "...." +" </p>"
+    type.play()
+})
+
+socket.on('newconnection' , (data)=>{
+    console.log(data)
+    usercount.innerHTML= "<p>" + data + "</p>"
+})
