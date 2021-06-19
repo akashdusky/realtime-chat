a1f13af2ab7e52f9dd90da7aaa4258d9660fff4d
akashdusky <32032227+akashdusky@users.noreply.github.com>
2021-06-20 04:52:09 +0530

    scx
    xdc

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const app = express();
 app.use(cors({
     origin : "*",
 }))
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    return next();
+  });
 console.log("cor enables ...")
 console.log(cors)
 
